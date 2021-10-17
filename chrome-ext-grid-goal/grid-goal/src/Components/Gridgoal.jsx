be25f6a11e716e2055d18ee6dd3d40ba2858fc79
Scott Laughlin <scott.laughlin@yourbrandlive.com>
2021-10-16 22:36:38 -0700

    Ugh, I don't even know. Maybe some resizing issues

--- chrome-ext-grid-goal/grid-goal/src/Components/Gridgoal.jsx
+++ chrome-ext-grid-goal/grid-goal/src/Components/Gridgoal.jsx
@@ -1,53 +1,66 @@
 import React, { useState, useEffect } from "react";
+
 import addIcon from '../assets/add-amount.png';
-import { getGridDims, mapNumber } from '../utils/utils';
+import { getGridDims, mapNumber,  getNumColumns, listAllResizeEventListeners} from '../utils/utils';
 import GridgoalCell from '../Components/GridgoalCell';
 
 function setCellSize(boxW, numCols) {
 
 }
-function handleResize(numCols,   numCells) {
+function handleResize(goal) {
     let gridContainer = document.querySelector('.grid-goal-body-content')
-    if(!gridContainer){
+    if(!gridContainer || !goal){
         return
     }
-    
+
+    // window.removeEventListener('resize', () => { handleResize(goal)} );
+
     let gridDims = gridContainer.getBoundingClientRect();
-    
     let gridWidth = gridDims.width;
     let gridHeight = gridDims.height;
 
-
+    let numCols = getNumColumns(goal.numCells, gridWidth, gridHeight-gridHeight*.2)
+    // let numCols = 20;
+    // console.log(numCols)
     let cellSize = Math.max(30,(gridWidth - 5 * (numCols - 1)) / numCols);  
-
-    cellSize = Math.min(cellSize,100)
-
+    // cellSize = Math.min(cellSize,100)
+    
     let root = document.documentElement;
-    let fontSize = mapNumber(gridDims[2], 0, 900, 20, 10)
-
+    let fontSize = mapNumber(goal.numCells, 0, 400, 20, 10)
+    
     root.style.setProperty('--cell-font-size', fontSize + "px");
     root.style.setProperty('--cell-width', cellSize + "px");
+
+    
 
 }
 function Gridgoal( {selectedGoal} ) {
     
     const [goal, setGoal] = useState(selectedGoal);
-    const [numCells, setNumCells] = useState(String(goal.value));
+    const [numCells, setNumCells] = useState(String(goal.numCells));
    
 
     useEffect(() => {
-        const dimensions = getGridDims(numCells);
+        const dimensions = getGridDims(goal.numCells);
         // setNumCols(dimensions[0]);
         // setNumRows(dimensions[1]);
 
-        handleResize(dimensions[0])
+        // handleResize(goal)
+     
+
         setNumCells(String(goal.value));
         setGoal(selectedGoal)
-        window.addEventListener('resize', () => { handleResize(dimensions[0],  dimensions[2] ) });
-    
+
+        window.addEventListener('resize', () => { handleResize(selectedGoal) });
+        // console.log( listAllResizeEventListeners())
+        
     }, [selectedGoal])
 
-    // handleResize(numCols, setNumCols, setNumRows, numCells );
+    
+    handleResize(selectedGoal)
+    
+
+
 
     return (
         <div className="grid-goal-container">
--- chrome-ext-grid-goal/grid-goal/src/utils/utils.jsx
+++ chrome-ext-grid-goal/grid-goal/src/utils/utils.jsx
@@ -21,27 +21,43 @@ export const randomGoals = (arr, n) => {
     return result;
 };
 
-export const getGridMultiplier = (num) =>{
+export const getGridMultiplier = (num) => {
     // The value is entered as a string
     const gridMultiplier = num > 200 ? Math.ceil(num / 200) : 1;
     return gridMultiplier
 }
 
-export const getGridDims= (value) => {
+export const getNumColumns = (cellCount, bw, bh) => {
+    // let allowedColumnNums = [1, 2, 3, 4, 5, 10, 20, 25, 30, 40, 50, 60, 70, 75, 100];
+
+    let proportion = bh / bw;
+    // let allowedColumnNums = [1, 2, 3, 4, 5, 10, 20, 25, 30];
+    let allowedColumnNums = [...Array(30).keys()];
+
+    let x = Math.floor(Math.sqrt(cellCount/ proportion));
+
+    const closest = allowedColumnNums.reduce((prev, curr) =>
+        Math.abs(curr - x) < Math.abs(prev - x) ? curr : prev
+    );
+
+    // console.log(`closest, x`, closest, x, cellCount)
+
+    return closest
+}
+export const getGridDims = (value) => {
     // The value is entered as a string
 
     let allowedColumnNums = [1, 2, 3, 4, 5, 10, 20, 25, 30, 40, 50, 60, 70, 75, 100];
-    let gridsizeToMultiplier={
 
-    }
-    let num = value.replace(/,/g, "");
+    let num = value;
     const gridMultiplier = num > 200 ? Math.ceil(num / 200) : 1;
     num = Math.ceil(num / gridMultiplier);
 
 
-    let ratio = 1/1.3;
-    
+    let ratio = 1 / 1.3;
+
     let x = Math.sqrt(num / ratio);
+
 
     const closest = allowedColumnNums.reduce((prev, curr) =>
         Math.abs(curr - x) < Math.abs(prev - x) ? curr : prev
@@ -52,13 +68,33 @@ export const getGridDims= (value) => {
 };
 
 
-export const getGridDims_new = (value, boxW, boxH) => {
-    let allowedColumnNums = [1, 2, 3, 4, 5, 10, 20, 25, 30, 40, 50, 60, 70, 75, 100];
-    const gridMultiplier = value > 1000 ? Math.ceil(value / 1000) : 1;
-    
 
-
-    let ratio = boxW/boxH;
-    
-    let x = Math.sqrt(value / ratio);
-}
+export function listAllResizeEventListeners() {
+    const allElements = Array.prototype.slice.call(document.querySelectorAll('*'));
+    allElements.push(document);
+    allElements.push(window);
+  
+    const types = [];
+  
+    for (let ev in window) {
+      if (/^on/.test(ev)) types[types.length] = ev;
+    }
+  
+    let elements = [];
+    for (let i = 0; i < allElements.length; i++) {
+      const currentElement = allElements[i];
+      for (let j = 0; j < types.length; j++) {
+        if (typeof currentElement[types[j]] === 'function') {
+          elements.push({
+            "node": currentElement,
+            "type": types[j],
+            "func": currentElement[types[j]].toString(),
+          });
+        }
+      }
+    }
+  
+    return elements.sort(function(a,b) {
+      return a.type.localeCompare(b.type);
+    });
+  }
