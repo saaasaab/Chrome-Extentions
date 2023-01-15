b565849cfe62ea6a59773fd96413f342bb8f69bc
Scott Laughlin <scott.laughlin@yourbrandlive.com>
2023-01-14 22:27:43 -0800

    add tab functionality

--- chrome-ext-action-plan/popup.js
+++ chrome-ext-action-plan/popup.js
@@ -1,15 +1,39 @@
-const dateControl = document.querySelector('input[type="date"]');
-dateControl.value = '2017-06-01';
-console.log(dateControl.value); // prints "2017-06-01"
-console.log(dateControl.valueAsNumber); // prints 1496275200000, 
+// const dateControl = document.querySelector('input[type="date"]');
+// dateControl.value = '2017-06-01';
+// console.log(dateControl.value); // prints "2017-06-01"
+// console.log(dateControl.valueAsNumber); // prints 1496275200000, 
+const plans = [{
+    title:'',
+    start_date: '',
+    due_date:0,
+    completed: false,
+    objectives:'',
+    notes: [],
+    action_items:[
+        {
+            item:'',
+            due:''
+        }
+    ]
+}]
 
-let messageIndex;
-try {
-    messageIndex = JSON.parse(new URLSearchParams(location.search).get('index'));
-} catch (e) {}
 
-// simplify the displayed URL in the address bar
+const tabs = document.querySelectorAll(".ap-tab");
 
-let deMoElement = document.querySelector('.de-mo-quote');
-const deMoQuoteIndex = messageIndex ?? Math.floor(Math.random() * demotivationalQuotes.length)
-deMoElement.textContent = demotivationalQuotes[deMoQuoteIndex]
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    const tabId = tab.getAttribute("id");
+    const tabContent = document.querySelector(`#${tabId}-content`);
+
+    tabs.forEach(tab => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+
+    const tabPanes = document.querySelectorAll(".tab-pane");
+    tabPanes.forEach(tabPane => {
+      tabPane.classList.remove("active");
+    });
+    tabContent.classList.add("active");
+  });
+});
