699fbb14b417d31903f7f992f29038fecb2ddbc6
Scott Laughlin <scott.laughlin@yourbrandlive.com>
2023-01-14 23:11:54 -0800

    get local storage hooked up

--- chrome-ext-action-plan/popup.js
+++ chrome-ext-action-plan/popup.js
@@ -1,11 +1,7 @@
-// const dateControl = document.querySelector('input[type="date"]');
-// dateControl.value = '2017-06-01';
-// console.log(dateControl.value); // prints "2017-06-01"
-// console.log(dateControl.valueAsNumber); // prints 1496275200000, 
-const plans = [{
+const defaultPlans = [{
     title:'',
     start_date: '',
-    due_date:0,
+    due_date:'',
     completed: false,
     objectives:'',
     notes: [],
@@ -16,6 +12,54 @@ const plans = [{
         }
     ]
 }]
+const allPlans = JSON.parse(window.localStorage.getItem('ap-all-plans')) || defaultPlans;
+
+function saveInLocal(stateTitle, stateValues) {
+    window.localStorage.setItem(stateTitle, JSON.stringify(stateValues));
+}
+
+function loadPreSetValues(){
+    console.log(`allPlans[activeIndex].title`, allPlans[activeIndex])
+    document.getElementById("ap-project-name").value = allPlans[activeIndex].title;
+    document.getElementById("ap-start-date").value = allPlans[activeIndex].start_date;
+    document.getElementById("ap-due-date").value = allPlans[activeIndex].due_date;
+    document.getElementById("ap-budget").value = allPlans[activeIndex].budget;
+    document.getElementById("ap-completed").checked = allPlans[activeIndex].completed;
+
+}
+
+
+// const dateControl = document.querySelector('input[type="date"]');
+// dateControl.value = '2017-06-01';
+// console.log(dateControl.value); // prints "2017-06-01"
+// console.log(dateControl.valueAsNumber); // prints 1496275200000, 
+
+
+let activeIndex = 0;
+
+function handleNameChange(e) {
+    allPlans[activeIndex].title = e.target.value;
+    saveInLocal('ap-all-plans',allPlans);
+    console.log(`allPlans`, allPlans)
+}
+function handleStartDateChange(e) {
+    allPlans[activeIndex].start_date = e.target.value;
+    saveInLocal('ap-all-plans',allPlans);
+}
+function handleDueDateChange(e) {
+    allPlans[activeIndex].due_date= e.target.value;
+    saveInLocal('ap-all-plans',allPlans);
+}
+function handleBudgetChange(e) {
+    allPlans[activeIndex].budget= e.target.value;
+    saveInLocal('ap-all-plans',allPlans);
+}
+function handleCompletedChange(e) {
+    allPlans[activeIndex].completed = e.target.checked;
+    console.log(`e.target.value`, e.target.checked)
+    saveInLocal('ap-all-plans',allPlans);
+}
+
 
 
 const tabs = document.querySelectorAll(".ap-tab");
@@ -36,4 +80,26 @@ tabs.forEach(tab => {
     });
     tabContent.classList.add("active");
   });
+});
+
+
+
+
+
+
+// saveInLocal('dc-active-states', JSON.stringify(activeStates));
+
+window.addEventListener('load', () => {
+   
+    loadPreSetValues();
+
+    // loads existing data from local storage.
+
+    // Project Overview
+    document.getElementById("ap-project-name").addEventListener("change", handleNameChange);
+    document.getElementById("ap-start-date").addEventListener("change", handleStartDateChange);
+    document.getElementById("ap-due-date").addEventListener("change", handleDueDateChange);
+    document.getElementById("ap-budget").addEventListener("change", handleBudgetChange);
+    document.getElementById("ap-completed").addEventListener("change", handleCompletedChange);
+
 });
