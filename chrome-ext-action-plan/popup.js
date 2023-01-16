2d78b9efd87921cc8566e62cd51ec838af550226
Scott Laughlin <scott.laughlin@yourbrandlive.com>
2023-01-15 20:58:40 -0800

    fix the conditional formatting issue and fix the tabs issue

--- chrome-ext-action-plan/popup.js
+++ chrome-ext-action-plan/popup.js
@@ -74,20 +74,18 @@ const defaultPlan = {
     ]
 };
 
-let allPlans = (JSON.parse(window.localStorage.getItem('ap-all-plans')) || [defaultPlan]);
+let allPlans = (JSON.parse(window.localStorage.getItem('ap-all-plans')) || [{...defaultPlan}]);
+
 
 let activeIndex = 0;
-const actionItems = allPlans[activeIndex].action_items;
 
-
-actionItems?.sort((a,b) => Number(a.due) - Number(b.due));
+// const actionItems = allPlans[activeIndex].action_items;
+// actionItems?.sort((a,b) => Number(a.due) - Number(b.due));
+// allPlans[activeIndex].action_items = [...actionItems]
 
 function saveInLocal(stateTitle, stateValues) {
     window.localStorage.setItem(stateTitle, JSON.stringify(stateValues));
 }
-
-
-
 
 function loadPreSetValues(){
     const activePlan = allPlans[activeIndex];
@@ -108,10 +106,8 @@ function loadPreSetValues(){
 
 function handleNameChange(e) {
     allPlans[activeIndex].title = e.target.value;
-
     const tab = document.querySelector(`#tab${activeIndex}`);
     tab.innerHTML = e.target.value;
-
     saveInLocal('ap-all-plans',allPlans);
 }
 function handleStartDateChange(e) {
@@ -138,7 +134,6 @@ function handleNotesChange(e) {
     allPlans[activeIndex].notes=e.target.value;
     saveInLocal('ap-all-plans',allPlans);
 }
-
 function handleActionTitleChange(e, i) {
     allPlans[activeIndex].action_items[i].item =e.target.value;
     saveInLocal('ap-all-plans',allPlans);
@@ -155,41 +150,74 @@ function handleActionCompleteChange(e, i) {
 
 function resetTabs(){
     document.querySelector('.tab-header').innerHTML=`<img class="ap-add-tab" src="add-icon.svg" alt="Add new tab SVG" />`;
-    
     allPlans.forEach((plan,i)=>{
         addTabsToPage(plan,i);
     });
 }
+
+function resetFields(){
+    document.querySelectorAll('input[type=checkbox]').forEach(input => input.checked === false);
+    document.querySelectorAll('input[type=text]').forEach(input => input.value === '')
+    document.querySelectorAll('textarea').forEach( input => input.value === '')
+}
+
 function addTab(){
-    allPlans.push(defaultPlan);
+    allPlans.push({...defaultPlan});
     saveInLocal('ap-all-plans',allPlans);
+
     activeIndex = allPlans.length - 1;
     resetTabs();
+    resetFields();
     addTabFunctionality()
     loadPreSetValues();
     activateHandlers();
+    addConditionalFormatting()
 }
 
-function addConditionalFormatting() {
-    const tabs = document.querySelectorAll(".ap-action-item-date");
-    var todayDate = new Date().toISOString().slice(0, 10);
+function toDate(dateStr){
+    const [day, month, year] = dateStr.split("-")
+    return new Date(year, month - 1, day)
+  }
 
-    tabs.forEach(tab=>{
-        if(!tab.value){
-            tab.style.backgroundColor = "var(--white)"
+function addConditionalFormatting() {
+    // const dateFormatted = new Date().toISOString().slice(0, 10)
+
+    const dateFormatted = new Date().toLocaleDateString().slice(0, 10)
+    const dates = document.querySelectorAll(".ap-action-item-date");
+
+    var todayDate = new Date(dateFormatted).getTime();
+
+   
+     // ascertain the timezone offset
+    const timeOffset = (new Date()).getTimezoneOffset() * 60 * 1000
+
+
+    dates.forEach(date=>{
+         // compensate for the weirdness
+         const milliseconds = date.valueAsNumber + timeOffset
+         // make a real date object
+         const inputDate = new Date(milliseconds)
+        
+        const dueDate = new Date(inputDate).toLocaleDateString().slice(0, 10);
+        const newDate = new Date(dueDate).getTime();
+
+        if(!date.value){
+            date.style.backgroundColor = "var(--white)"
         }
         // EXPIRED
-        else if(tab.value < todayDate){
-            tab.style.backgroundColor = "#ffe4e4"
+        else if( newDate < todayDate){
+            date.style.backgroundColor = "#ffe4e4"
         }
         // DUE
-        else if(tab.value === todayDate){
-            tab.style.backgroundColor = "#fdfdd1"
+        else if( newDate === todayDate){
+            date.style.backgroundColor = "#fdfdd1"
         }
         // UPCOMING
-        else if(tab.value > todayDate){
-            tab.style.backgroundColor = "#d4ffd4"
+        else if( newDate > todayDate){
+            date.style.backgroundColor = "#d4ffd4"
         }
+
+
     })
 }
 
@@ -198,8 +226,6 @@ function addTabsToPage(plan, i){
     const tabHTML =  `<div class="ap-tab ${isActive}" id="tab${i}">${plan.title || 'New Tab'}</div>`
     document.querySelector('.ap-add-tab').insertAdjacentHTML("beforebegin",tabHTML);
     document.querySelector(".ap-add-tab").addEventListener("click", addTab);
-    // 
-//  <div class="tab-header">
 }
 
 
@@ -208,7 +234,7 @@ function addTabFunctionality(){
     tabs.forEach((tab,i) => {
         
         tab.addEventListener("click", () => {
-            activeIndex= i;
+            activeIndex = i;
             loadPreSetValues()
             activateHandlers();
             const tabId = tab.getAttribute("id");
@@ -225,8 +251,6 @@ function addTabFunctionality(){
             });
 
             addConditionalFormatting()
-
-
             // tabContent.classList.add("active");
         });
           
@@ -247,9 +271,6 @@ function addTabFunctionality(){
 
 }
 
-
-
-
 function  activateHandlers(){
     document.getElementById("ap-project-name").addEventListener("change", handleNameChange);
     document.getElementById("ap-start-date").addEventListener("change", handleStartDateChange);
@@ -258,15 +279,13 @@ function  activateHandlers(){
     document.getElementById("ap-completed").addEventListener("change", handleCompletedChange);
     document.getElementById("ap-project-objectives").addEventListener("change", handleObjectivesChange);
     document.getElementById("ap-notes-and-ideas").addEventListener("change", handleNotesChange);
-
     document.querySelector(".ap-add-tab").addEventListener("click", addTab);
 
     Array.from(Array(13).keys()).forEach((item,i)=>{
         document.getElementById(`ap-action-item-title-${i+1}`).addEventListener('change',(e)=>{handleActionTitleChange(e,i)});
         document.getElementById(`ap-action-item-date-${i+1}`).addEventListener('change',(e)=>{handleActionDateChange(e,i)});
         document.getElementById(`ap-action-item-completed-${i+1}`).addEventListener('change',(e)=>{handleActionCompleteChange(e,i)});
-        // document.getElementById(`ap-action-item-date-${i+1}`).value = allPlans[activeIndex].action_items[i].due;
-        // document.getElementById(`ap-action-item-completed-${i+1}`).value = allPlans[activeIndex].action_items[i].done;
+
     })
 
 }
@@ -278,13 +297,14 @@ window.addEventListener('load', () => {
     // loads existing data from local storage.
     loadPreSetValues();
 
-    activateHandlers()
+
+    activateHandlers();
     
     allPlans.forEach((plan,i)=>{
         addTabsToPage(plan,i);
     })
 
-    addTabFunctionality()
+    addTabFunctionality();
 
-    addConditionalFormatting()
+    addConditionalFormatting();
 });
